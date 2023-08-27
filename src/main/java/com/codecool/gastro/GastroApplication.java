36a02203c829639c96df294ddcc51400612b31b2
Kacper Tomczyk <46459137+Skinnero@users.noreply.github.com>
2023-08-27 19:17:22 +0200

    first init

---
+++ src/main/java/com/codecool/gastro/GastroApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.gastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecool/gastro/GastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.gastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
