453c5f5fab2cdc9088bec1caff8dc5e60a64d9de
sebgaw12 <sebastian.gawrys1@gmail.com>
2023-08-30 10:35:50 +0200

    ingredient service

---
+++ src/main/java/com/codecool/gastro/service/IngredientService.java
@@ -0,0 +1,26 @@
+package com.codecool.gastro.service;
+
+import com.codecool.gastro.controller.dto.ingredientDto.IngredientDto;
+import com.codecool.gastro.controller.dto.ingredientDto.NewIngredientDto;
+import com.codecool.gastro.mapper.IngredientMapper;
+import com.codecool.gastro.repository.IngredientRepository;
+import com.codecool.gastro.repository.entity.Ingredient;
+import org.springframework.stereotype.Service;
+
+@Service
+public class IngredientService {
+
+    private final IngredientRepository ingredientRepository;
+    private final IngredientMapper ingredientMapper;
+
+    public IngredientService(IngredientRepository ingredientRepository, IngredientMapper ingredientMapper) {
+        this.ingredientRepository = ingredientRepository;
+        this.ingredientMapper = ingredientMapper;
+    }
+
+    public IngredientDto saveNewIngredient(NewIngredientDto ingredientDto){
+        Ingredient newIngredient = ingredientRepository
+                .save(ingredientMapper.mapDtoToIngredient(ingredientDto));
+        return ingredientMapper.mapIngredientToDto(newIngredient);
+    }
+}
