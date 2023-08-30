83ac7524355a1b1c892ed78a4149a77cc2c0271c
sebgaw12 <sebastian.gawrys1@gmail.com>
2023-08-30 10:21:23 +0200

    ingredient entity and repository interface

---
+++ src/main/java/com/codecool/gastro/repository/IngredientRepository.java
@@ -0,0 +1,11 @@
+package com.codecool.gastro.repository;
+
+import com.codecool.gastro.repository.entity.Ingredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface IngredientRepository extends JpaRepository<Ingredient, UUID> {
+}
---
+++ src/main/java/com/codecool/gastro/repository/entity/Ingredient.java
@@ -0,0 +1,48 @@
+package com.codecool.gastro.repository.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank
+    private String name;
+
+    @ManyToMany(mappedBy = "restaurantMenu")
+    private Set<RestaurantMenu> restaurantMenus;
+
+    public Ingredient() {
+    }
+
+    public Ingredient(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<RestaurantMenu> getRestaurantMenus() {
+        return restaurantMenus;
+    }
+
+    public void setRestaurantMenus(Set<RestaurantMenu> restaurantMenus) {
+        this.restaurantMenus = restaurantMenus;
+    }
+}
