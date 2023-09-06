d5d21af4dc17c531240f8fe351cbcaa28e20c0ad
FoXcodeZ <m.grabowy@int.pl>
2023-09-06 16:47:39 +0200

    fixed wrong bean annotation in CustomerDto. (@NotBlank -> @Id)

--- src/main/java/com/codecool/gastro/dto/customer/CustomerDto.java
+++ src/main/java/com/codecool/gastro/dto/customer/CustomerDto.java
@@ -1,4 +1,5 @@
 package com.codecool.gastro.dto.customer;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -6,7 +7,7 @@ import jakarta.validation.constraints.Size;
 import java.util.UUID;
 
 public record CustomerDto(
-        @NotBlank(message = "customer need an identifier")
+        @Id
         UUID id,
 
         @NotBlank(message = "forename cannot be empty")
