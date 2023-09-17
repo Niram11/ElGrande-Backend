e4896e6b5acad628d3f0d5e3a254b58be4944ef2
Kacper Tomczyk <46459137+Skinnero@users.noreply.github.com>
2023-09-17 10:18:46 +0200

    Adding: RestuaruantRepositoryTest along with h2 and flyway dependencies to pom and data for flyway for createing db

---
+++ src/test/java/com/codecool/gastro/repository/RestaurantRepositoryTest.java
@@ -0,0 +1,114 @@
+package com.codecool.gastro.repository;
+
+import com.codecool.gastro.repository.entity.Restaurant;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class RestaurantRepositoryTest {
+
+    @Autowired
+    private RestaurantRepository repository;
+
+    @Test
+    void testFindAll_ShouldReturnListOfRestaurants_WhenCallingMethod() {
+        // when
+        List<Restaurant> list = repository.findAll();
+
+        // test
+        assertEquals(list.size(), 2);
+    }
+
+    @Test
+    void testFindById_ShouldReturnRestaurant_WhenExist() {
+        // given
+        UUID id = UUID.fromString("4e99a0c4-d1bb-48c6-95f3-d202b84d1dc5");
+
+        // when
+        Optional<Restaurant> restaurant = repository.findById(id);
+
+        // test
+        assertTrue(restaurant.isPresent());
+    }
+
+    @Test
+    void testFindById_ShouldReturnEmptyOptional_WhenNoRestaurant() {
+        // given
+        UUID id = UUID.randomUUID();
+
+        // when
+        Optional<Restaurant> restaurant = repository.findById(id);
+
+        // test
+        assertTrue(restaurant.isEmpty());
+    }
+
+    @Test
+    void testFindById_ShouldReturnEmptyOptional_WhenProvidingExistingRestaurantsIdButIsDeleted() {
+        // given
+        UUID id = UUID.fromString("4e99a0c4-d1bb-48c6-95f3-d202b84d1dc5");
+
+        // when
+        Restaurant restaurant = repository.findById(id).get();
+        restaurant.setDeleted(true);
+
+        repository.save(restaurant);
+        Optional<Restaurant> afterDeletionRestaurant = repository.findById(id);
+
+        // test
+        assertTrue(afterDeletionRestaurant.isEmpty());
+    }
+
+
+    @Test
+    void testSave_ShouldReturnSameEntityAndRestaurant_WhenSavingValidRestaurant() {
+        // given
+        Restaurant restaurant = new Restaurant();
+        restaurant.setName("Kacper");
+        restaurant.setDescription("Kacper");
+        restaurant.setWebsite("Kacper");
+        restaurant.setContactNumber(123123123);
+        restaurant.setContactEmail("Kacper@wp.pl");
+
+        // when
+        Restaurant savedRestaurant = repository.save(restaurant);
+        Optional<Restaurant> savedRestaurantById = repository.findById(savedRestaurant.getId());
+
+        // test
+        assertTrue(savedRestaurantById.isPresent());
+        assertEquals(savedRestaurant, restaurant);
+    }
+
+    @Test
+    void testSave_ShouldUpdateRestaurant_WhenProvidingExistingId() {
+        // given
+        UUID id = UUID.fromString("4e99a0c4-d1bb-48c6-95f3-d202b84d1dc5");
+
+        Restaurant restaurant = new Restaurant();
+        restaurant.setId(id);
+        restaurant.setName("KacperTest");
+        restaurant.setDescription("KacperTest");
+        restaurant.setWebsite("KacperTest");
+        restaurant.setContactNumber(123123123);
+        restaurant.setContactEmail("KacperTest@wp.pl");
+
+        // when
+        Restaurant restaurantAfterSave = repository.save(restaurant);
+
+        // test
+        assertEquals(restaurantAfterSave.getId(), id);
+        assertNotEquals(restaurantAfterSave.getName(), "Kacper");
+        assertNotEquals(restaurantAfterSave.getDescription(), "Desc1");
+        assertNotEquals(restaurantAfterSave.getWebsite(), "Kacper.pl");
+        assertNotEquals(restaurantAfterSave.getContactNumber(), 123);
+        assertNotEquals(restaurantAfterSave.getContactEmail(), "Kacper@wp.pl");
+    }
+}
---
+++ src/test/resources/db/migration/V1_1__test_data.sql
@@ -0,0 +1,3 @@
+insert into restaurant(id, name, description, website, contact_number, contact_email, is_deleted)
+values ('4e99a0c4-d1bb-48c6-95f3-d202b84d1dc5', 'Kacper', 'Desc1', 'Kacper.pl', 123, 'Kacper@wp.pl', false),
+       ('860a757c-a704-4ac3-820c-2f3f37657a68', 'Tomek', 'Decs2', 'Tomek.pl', 321, 'Tomek@wp.pl', false);
